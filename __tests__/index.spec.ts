17c6c902eb9aaa3cc82017ebe1e2d18898939177
afeiship <1290657123@qq.com>
2025-05-13 11:11:22 +0800

    fix: rm jest

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,10 +1,11 @@
 import HotConfig from '../src';
 
+// https://student-api.beta.saybot.net/api/v2/configurations/HOMEWORK_NG_WEB
 const hotConfig = new HotConfig({
   envs: {
     beta: 'https://student-api.beta.saybot.net/api/v2/configurations',
   },
-  path: 'HOMEWORK_NG_WEB',
+  path: '/HOMEWORK_NG_WEB',
   env: 'beta',
 });
 
@@ -12,6 +13,6 @@ describe('api.basic', () => {
   test('normail single value case', async () => {
     await hotConfig.fetch();
     const res = hotConfig.get();
-    console.log('res: ', res);
+    expect(res.success).toBe(true);
   });
 });
